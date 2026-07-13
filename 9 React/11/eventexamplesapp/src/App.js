d844f97a2ecfb3d5f875fb11d4ed224f4a0f845b
Amit Kumar Deo <159774907+22AmitDeo@users.noreply.github.com>
2026-07-13 22:00:20 +0530

    React Ex 11

---
+++ 9 React/11/eventexamplesapp/src/App.js
@@ -0,0 +1,19 @@
+import Counter from "./Components/Counter";
+import CurrencyConvertor from "./Components/CurrencyConvertor";
+
+function App() {
+
+  return (
+    <div style={{ padding: "20px" }}>
+
+      <Counter />
+
+      <hr />
+
+      <CurrencyConvertor />
+
+    </div>
+  );
+}
+
+export default App;
